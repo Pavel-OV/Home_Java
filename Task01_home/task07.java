50313cf369fd19023ad41c2360f2a8dff71268be
Pavel <black_yaguar@mail.ru>
2023-03-08 01:45:53 +0500

    Решение третьей задачм домашней работы

--- Task01_home/task07.java
+++ Task01_home/task07.java
@@ -3,16 +3,36 @@
 // Ввод знака ->
 // Ввод числа ->
 
-
 public class task07 {
     public static void main(String[] args) {
-        
 
-    System.out.println("Реализовать простой калькулятор (+ - / *)");
-    int a =input.Int("Введите число a");
-    int znak =input.Int("Арифметическое действие");
-    int b =input.Int("Введите число b ");
-
+        System.out.println("Реализовать простой калькулятор (+ - / *)");
+        double a = input.Double("Введите число a");
+        char math_znak = input.Str("Выберите действие (+ - / *)").charAt(0);
+        double b = input.Double("Введите число b ");
+        double ud_pv = 0;
+        switch (math_znak) {
+            case '*':
+                ud_pv = a * b;
+                break;
+            case '/':
+                ud_pv = a / b;
+                break;
+            case '+':
+                ud_pv = a + b;
+                break;
+            case '-':
+                ud_pv = a - b;
+                break;
+            default:
+                System.out.printf("Ошибка! Введите арифметический знак(+ - / *)");
+                return;
+        }
+        System.out.printf("Результат вычислений "); 
+        System.out.printf(a+" "+math_znak+" "+b+" = "+ud_pv); 
     }
-    
+
+    private static char charAt(int i) {
+        return 0;
+    }
 }
