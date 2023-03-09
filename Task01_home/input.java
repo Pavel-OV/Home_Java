350e68a0c5cf1e7ba432424d1a4e13541ea83a80
Pavel <black_yaguar@mail.ru>
2023-03-09 17:28:46 +0500

    Первая домашняя работа

--- Task01_home/input.java
+++ Task01_home/input.java
@@ -21,5 +21,3 @@ public class input {
         }
     }
     
-    
- // String users_string = input.Str("Введите строку");        System.out.println(cp866(users_string));    }    public static String cp866 (String users_string){        int counter = 1;        StringBuilder users_cp866 = new StringBuilder();        for (int i = 0; i < users_string.length()-1; i++) {            if (users_string.charAt(i)==users_string.charAt(i+1)){                counter++;            }            else {                if (counter==1){                    users_cp866.append(users_string.charAt(i));
