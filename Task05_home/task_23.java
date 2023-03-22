5f45333a44f426901c18fc4332ac9a570ed7905c
Pavel <black_yaguar@mail.ru>
2023-03-23 00:14:30 +0500

    Пятая домашняя работа

--- Task05_home/task_23.java
+++ Task05_home/task_23.java
@@ -1,5 +1,10 @@
 package Task05_home;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import Task02_home.input;
+
 public class task_23 {
 //     Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
 // Добавить функции 1) Добавление номера
@@ -22,31 +27,44 @@ public class task_23 {
 // Я: 2
 // Иванов: 1242353, 547568
 
-// Пусть дан список сотрудников:
-// Иван Иванов
-// Светлана Петрова
-// Кристина Белова
-// Анна Мусина
-// Анна Крутова
-// Иван Юрин
-// Петр Лыков
-// Павел Чернов
-// Петр Чернышов
-// Мария Федорова
-// Марина Светлова
-// Мария Савина
-// Мария Рыкова
-// Марина Лугова
-// Анна Владимирова
-// Иван Мечников
-// Петр Петин
-// Иван Ежов
+
 
 // Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
 // Отсортировать по убыванию популярности Имени.
-public static void main(String[] args) {
-    HashMap 
-}
-
+ public static void main(String[] args) {
+  System.out.println("структуру телефонной книги с помощью HashMap");
+  
     
+  Map<Integer, String> map = new HashMap<>();
+  map = pasport();
+  
+  for (Map.Entry<Integer,String> item: map.entrySet()){
+    if(item.getValue().equals("Иванов"))
+    System.out.println("Иванов "+item.getKey())  ;  
+  }
+  for (Map.Entry<Integer,String> item: map.entrySet()){
+    //if(item.getKey()== 234561 )
+    System.out.printf("Номер: %s Фамилия %s\n",item.getKey(),item.getValue());
+  }
+
+
+ }
+
+
+  public static HashMap<Integer, String> pasport() {
+  HashMap<Integer, String> pasport = new HashMap<>();
+  pasport.put(123456, "Иванов");
+  pasport.put(321456, "Васильев");
+  pasport.put(234561, "Петрова");
+  pasport.put(234432, "Иванов");
+  pasport.put(654321,"Петрова");
+  pasport.put(345678, "Иванов");  
+  int number = 1;
+  while(number !=0){
+    number = input.Int("Введите номер паспорта");
+    String surname = input.Str("Введите фамилию");
+    pasport.put(number, surname);
+  }
+  return pasport;
+}
 }
