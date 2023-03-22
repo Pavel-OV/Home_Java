a53572ff365605562b715b32c257f82b02710fdc
Pavel <black_yaguar@mail.ru>
2023-03-23 00:16:53 +0500

    Вторая домашняя работа

--- Task02_home/task09.java
+++ Task02_home/task09.java
@@ -3,7 +3,6 @@
 // Реализуйте алгоритм сортировки пузырьком числового массива (введён вами),
 // результат после каждой итерации запишите в лог-файл.
 
-// Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение). Без встр. функций
 // 4*дополнительно. К калькулятору из предыдущего дз добавить логирование.
 // 1 + 3 = 4
 
@@ -19,9 +18,14 @@
 // stroka == stroka1 ----> False
 
 package Task02_home;
+import java.io.File;
+import java.io.FileWriter;
 
 public class task09 {
     public static void main(String[] args) {
+          
+        String file_name = "D:\\Home_Java\\Task02_home\\1.txt";
+        File file = new File(file_name);
         System.out.println(" Введите число от 1 до 20, определяющее количество чисел");
         int size = input.Int("Введите число ");
         int array[] = new int[size];
@@ -29,27 +33,44 @@ public class task09 {
             array[i] = input.Int("Введите число ");
 
         }
+        try{
+            FileWriter writer = new FileWriter(file,true);
+            
         for (int i = 0; i < size; i++) {
+            writer.write(array[i]+" ");
             System.out.print(" " + array[i]);
         }
+        writer.close();
         System.out.println();
         boolean isSorted = false;
         int buf;
+        
         while (!isSorted) {
             isSorted = true;
             for (int i = 0; i < array.length - 1; i++) {
                 if (array[i] > array[i + 1]) {
                     isSorted = false;
-
                     buf = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = buf;
+                    
                 }
+                writer.write(array[i]+" ");
+                writer.write("\n");
+                
+
+               
             }
+            
         }
         for (int i = 0; i < size; i++) {
             System.out.print(" " + array[i]);
         }
+        writer.close();
+        }
+        catch (Exception e){
+            System.out.println("Что то пошло не так");
+            }
 
     }
 }
